d2324a9104d09c440121b7a0b278dd53d36d9344
nachosalvador <nachosalvador83@gmail.com>
2026-09-09 16:49:30 +0200

    Set up symfony mailer lite

--- web/sites/default/settings.custom.php
+++ web/sites/default/settings.custom.php
@@ -1,5 +1,8 @@
 <?php
 
+$config['symfony_mailer_lite.symfony_mailer_lite_transport.smtp']['configuration']['user'] = getenv('smtp__user');
+$config['symfony_mailer_lite.symfony_mailer_lite_transport.smtp']['configuration']['pass'] = getenv('smtp__pass');
+
 // Disable Lagoon logs if not on Amazee.
 if (!getenv('LAGOON')) {
   $config['lagoon_logs.settings']['disable'] = 1;
